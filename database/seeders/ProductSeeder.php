12506ef635949e6c366d632787bb43ef7196a2fb
George <george.rabus@isa.utm.md>
2026-01-23 09:22:40 +0200

    seeder rerunnable

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -142,7 +142,10 @@ class ProductSeeder extends Seeder
         ];
 
         foreach ($products as $product) {
-            Product::create($product);
+            Product::updateOrCreate(
+                ['slug' => $product['slug']],
+                $product
+            );
         }
     }
 }
